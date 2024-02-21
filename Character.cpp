52abbd28aa1daeeb1a0d552c45817737f0c7fabe
Eric Liu <eeric048@uottawa.ca>
2024-02-21 17:06:08 -0500

    worked on Character

--- Character.cpp
+++ Character.cpp
@@ -1,4 +1,281 @@
 #include <cstdlib> // For std::rand() and std::srand()
 #include <ctime>   // For std::time()
+#include <iostream>
+#include <string>
+#include <stdexcept>
+#include <math.h>
+#include <bits/stdc++.h>
 #include "Character.h"
 
+using namespace std;
+
+Character::Character(int level){
+    if (level > 0){
+        level = level;
+
+        strength = generateAbilityScores();
+        dexterity = generateAbilityScores();
+        constitution = generateAbilityScores();
+        intelligence = generateAbilityScores();
+        wisdom = generateAbilityScores();
+        charisma = generateAbilityScores();
+
+        strengthMod = calculateModifiers(strength);
+        dexterityMod = calculateModifiers(dexterity);
+        constitutionMod = calculateModifiers(constitution);
+        intelligenceMod = calculateModifiers(intelligence);
+        wisdomMod = calculateModifiers(wisdom);
+        charismaMod = calculateModifiers(charisma);
+
+        hitPoints = calculateHitPoints();
+        armorClass = calculateArmorClass();
+        attackBonus = calculateAttackBonus();
+        damageBonus = calculateDamageBonus();
+    }
+    else{
+        throw invalid_argument("Level must be positive");
+    }
+}
+int Character::generateAbilityScores(){
+    int n = 4;
+    int roll[n];
+    for (int i=0;i<n; i++){ // rolling 4d6
+        int high = 6;
+        int low = 1;
+        roll[i] = rand() % (high - low) + low;
+    }
+    sort(roll, roll + n); // dropping the lowest roll
+    return roll[1]+roll[2]+roll[3]; // summing the rest
+}
+int Character::calculateModifiers(int points){
+    float difference = points-10;
+    float modifier = difference/2;
+    return floor(modifier);
+}
+int Character::calculateHitPoints(){
+    return getConstitutionMod()+getLevel();
+}
+int Character::calculateArmorClass(){
+    return getDexterityMod();
+}
+int Character::calculateAttackBonus(){
+    return getLevel()+getStrengthMod()+getDexterityMod();
+}
+int Character::calculateDamageBonus(){
+    return getStrengthMod();
+}
+// Setter for level
+void Character::setLevel(int newLevel){
+    if (newLevel > 0){
+        level = newLevel;
+        recalculateAttributes();
+    }
+    else{
+        throw invalid_argument("Level must be positive");
+    }
+}
+void Character::setStrength(int newStrength){
+    if (newStrength >= 0){
+        strength = newStrength;
+    } 
+    else{
+        throw invalid_argument("Strength must be positive");
+    }
+}
+void Character::setDexterity(int newDexterity){
+    if (newDexterity >= 0){
+        dexterity = newDexterity;
+    }
+    else{
+        throw invalid_argument("Dexterity must be positive");
+    }
+}
+void Character::setConstitution(int newConstitution){
+    if (newConstitution >= 0){
+        constitution = newConstitution;
+    }
+    else{
+        throw invalid_argument("Constitution must be positive");
+    }
+}
+void Character::setIntelligence(int newIntelligence){
+    if (newIntelligence >= 0){
+        intelligence = newIntelligence;
+    }
+    else{
+        throw invalid_argument("Intelligence must be positive");
+    }
+}
+void Character::setWisdom(int newWisdom){
+    if (newWisdom >= 0){
+        wisdom = newWisdom;
+    }
+    else{
+        throw invalid_argument("Wisdom must be positive");
+    }
+}
+void Character::setCharisma(int newCharisma){
+    if (newCharisma >= 0){
+        charisma = newCharisma;
+    }
+    else{
+        throw invalid_argument("Charisma must be positive");
+    }
+}
+//increase (buff and debuff)
+void Character::increaseLevel(int levelUp){
+    if (level+levelUp > 0){
+        level+=levelUp;
+        recalculateAttributes();
+    }
+    else{
+        throw invalid_argument("New Level must be positive");
+    }
+}
+void Character::increaseStrength(int buffStrength){
+    if (strength + buffStrength >= 0){
+        strength += buffStrength;
+        strengthMod = calculateModifiers(strength);
+        recalculateAttributes();
+    } 
+    else{
+        throw invalid_argument("New Strength must be positive");
+    }
+}
+void Character::increaseDexterity(int buffDexterity){
+    if (dexterity + buffDexterity >= 0){
+        dexterity += buffDexterity;
+        dexterityMod = calculateModifiers(dexterity);
+        recalculateAttributes();
+    }
+    else{
+        throw invalid_argument("New Dexterity must be positive");
+    }
+}
+void Character::increaseConstitution(int buffConstitution){
+    if (constitution + buffConstitution >= 0){
+        constitution += buffConstitution;
+        constitutionMod = calculateModifiers(constitution);
+        recalculateAttributes();
+    }
+    else{
+        throw invalid_argument("New Constitution must be positive");
+    }
+}
+void Character::increaseIntelligence(int buffIntelligence){
+    if (intelligence + buffIntelligence >= 0){
+        intelligence += buffIntelligence;
+        intelligenceMod = calculateModifiers(intelligence);
+        recalculateAttributes();
+    }
+    else{
+        throw invalid_argument("New Intelligence must be positive");
+    }
+}
+void Character::increaseWisdom(int buffWisdom){
+    if (wisdom + buffWisdom >= 0){
+        wisdom += buffWisdom;
+        wisdomMod = calculateModifiers(wisdom);
+        recalculateAttributes();
+    }
+    else{
+        throw invalid_argument("New Wisdom must be positive");
+    }
+}
+void Character::increaseCharisma(int buffCharisma){
+    if (charisma + buffCharisma >= 0){
+        charisma += buffCharisma;
+        charismaMod = calculateModifiers(charisma);
+        recalculateAttributes();
+    }
+    else{
+        throw invalid_argument("New Charisma must be positive");
+    }
+}
+// Recalculate attributes that depend on level or modifiers
+void Character::recalculateAttributes(){
+    hitPoints = calculateHitPoints();
+    armorClass = calculateArmorClass();
+    attackBonus = calculateAttackBonus();
+    damageBonus = calculateDamageBonus();
+}
+//equip gear
+void Character::equipArmor(string newArmor){
+    armor = newArmor;
+}
+void Character::equipShield(string newShield){
+    shield = newShield;
+}
+void Character::equipWeapon(string newWeapon){
+    weapon = newWeapon;
+}
+void Character::equipBoots(string newBoots){
+    boots = newBoots;
+}
+void Character::equipRing(string newRing){
+    ring = newRing;
+}
+void Character::equipHelmet(string newHelmet){
+    helmet = newHelmet;
+}
+// getters for Character stats
+int Character::getLevel() const{return level;}
+int Character::getHitPoints() const{return hitPoints;}
+int Character::getArmorClass() const{return armorClass;}
+int Character::getAttackBonus() const{return attackBonus;}
+int Character::getDamageBonus() const{return damageBonus;}
+// getters for abilities
+int Character::getStrength() const{return strength;}
+int Character::getDexterity() const{return dexterity;}
+int Character::getConstitution() const{return constitution;}
+int Character::getIntelligence() const{return intelligence;}
+int Character::getWisdom() const{return wisdom;}
+int Character::getCharisma() const{return charisma;}
+// getters for abilitiy modifiers
+int Character::getStrengthMod() const{return strengthMod;}
+int Character::getDexterityMod() const{return dexterityMod;}
+int Character::getConstitutionMod() const{return constitutionMod;}
+int Character::getIntelligenceMod() const{return intelligenceMod;}
+int Character::getWisdomMod() const{return wisdomMod;}
+int Character::getCharismaMod() const{return charismaMod;}
+// getters for equipment
+string Character::getArmor() const{return armor;}
+string Character::getShield() const{return shield;}
+string Character::getWeapon() const{return weapon;}
+string Character::getBoots() const{return boots;}
+string Character::getRing() const{return ring;}
+string Character::getHelmet() const{return helmet;}
+
+void Character::printCharacter(){
+    // Print attributes
+        cout << "Level: " << getLevel() << "\n";
+        cout << "Hit Points: " << getHitPoints() << "\n";
+        cout << "Armor Class: " << getArmorClass() << "\n";
+        cout << "Attack Bonus: " << getAttackBonus() << "\n";
+        cout << "Damage Bonus: " << getDamageBonus() << "\n";
+        
+        // Print ability scores
+        cout << "Ability Scores:" << "\n";
+        cout << "  Strength: " << getStrength() << " (Mod: " << getStrengthMod() << ")" << "\n";
+        cout << "  Dexterity: " << getDexterity() << " (Mod: " << getDexterityMod() << ")" << "\n";
+        cout << "  Constitution: " << getConstitution() << " (Mod: " << getConstitutionMod() << ")" << "\n";
+        cout << "  Intelligence: " << getIntelligence() << " (Mod: " << getIntelligenceMod() << ")" << "\n";
+        cout << "  Wisdom: " << getWisdom() << " (Mod: " << getWisdomMod() << ")" << "\n";
+        cout << "  Charisma: " << getCharisma() << " (Mod: " << getCharismaMod() << ")" << "\n";
+
+        // Print equipment
+        cout << "Equipment:" << "\n";
+        cout << "  Armor: " << (getArmor().empty() ? "None" : getArmor()) << "\n";
+        cout << "  Shield: " << (getShield().empty() ? "None" : getShield()) << "\n";
+        cout << "  Weapon: " << (getWeapon().empty() ? "None" : getWeapon()) << "\n";
+        cout << "  Boots: " << (getBoots().empty() ? "None" : getBoots()) << "\n";
+        cout << "  Ring: " << (getRing().empty() ? "None" : getRing()) << "\n";
+        cout << "  Helmet: " << (getHelmet().empty() ? "None" : getHelmet()) << "\n";
+}
+
+int main() {
+    Character fighter(12); // Create a level 1 fighter
+    cout<<fighter.getLevel();
+    fighter.printCharacter(); // Print fighter details
+    return 0;
+}
--- Character.h
+++ Character.h
@@ -9,19 +9,61 @@ public:
     Character(int level); // Constructor declaration
     void printCharacter(); // Method to print character details
 
-private:
+    // equip gear
+    void equipArmor(string newArmor);
+    void equipShield(string newShield);
+    void equipWeapon(string newWeapon);
+    void equipBoots(string newBoots);
+    void equipRing(string newRing);
+    void equipHelmet(string newHelmet);
+    // getters
+    int getLevel() const;
+    int getHitPoints() const;
+    int getArmorClass() const;
+    int getAttackBonus() const;
+    int getDamageBonus() const;
+    // getters for abilities
+    int getStrength() const;
+    int getDexterity() const;
+    int getConstitution() const;
+    int getIntelligence() const;
+    int getWisdom() const;
+    int getCharisma() const;
+    // getters for abilitiy modifiers
+    int getStrengthMod() const;
+    int getDexterityMod() const;
+    int getConstitutionMod() const;
+    int getIntelligenceMod() const;
+    int getWisdomMod() const;
+    int getCharismaMod() const;
+    // getters for equipment
+    string getArmor() const;
+    string getShield() const;
+    string getWeapon() const;
+    string getBoots() const;
+    string getRing() const;
+    string getHelmet() const;
+
+private: // private!!!
     int level;
-    int hitPoints;
-    int armorClass;
-    int attackBonus;
-    int damageBonus;
-    // ability and their modifiers in an array of size 2
-    int strength[2];
-    int dexterity[2];
-    int constitution[2];
-    int intelligence[2];
-    int wisdom[2];
-    int charisma[2];
+    int hitPoints; // based on constitution modifier and level
+    int armorClass; // based on dexterity modifier
+    int attackBonus; // based level and strength/dexterity modifiers
+    int damageBonus; //based on strength modifier
+    // ability score
+    int strength;
+    int dexterity;
+    int constitution;
+    int intelligence;
+    int wisdom;
+    int charisma;
+    // ability modifier
+    int strengthMod;
+    int dexterityMod;
+    int constitutionMod;
+    int intelligenceMod;
+    int wisdomMod;
+    int charismaMod;
     // equipment
     string armor;
     string shield;
@@ -30,13 +72,31 @@ private:
     string ring;
     string helmet;
 
+    int generateAbilityScores(); // generated randomly
+    int calculateModifiers(int points); // default to 1
+    int calculateHitPoints(); // based on constitution modifier and level
+    int calculateArmorClass(); // based on dexterity modifier
+    int calculateAttackBonus(); // based level and strength modifiers and dexterity modifiers
+    int calculateDamageBonus(); // based on strength modifier
 
-    void generateAbilityScores(); // generated randomly
-    void calculateModifiers(); // 
-    void calculateHitPoints(); // based on constitution modifier and level
-    void calculateArmorClass(); // based on dexterity modifier
-    void calculateAttackBonus(); // based level and strength modifiers and dexterity modifiers
-    void calculateDamageBonus(); // based on strength modifier
-
+    // setters
+    void setLevel(int newLevel);
+    // setters for abilities
+    void setStrength(int newStrength);
+    void setDexterity(int newDexterity);
+    void setConstitution(int newConstitution);
+    void setIntelligence(int newIntelligence);
+    void setWisdom(int newWisdom);
+    void setCharisma(int newCharisma);
+    //increase (buff and debuff)
+    void increaseLevel(int levelUp);
+    void increaseStrength(int strengthUp);
+    void increaseDexterity(int dexterityUp);
+    void increaseConstitution(int constitutionUp);
+    void increaseIntelligence(int intelligenceUp);
+    void increaseWisdom(int wisdomUp);
+    void increaseCharisma(int charismaUp);
+    // Recalculate attributes that depend on level or modifiers
+    void recalculateAttributes();
 };
 #endif // CHARACTER_H
