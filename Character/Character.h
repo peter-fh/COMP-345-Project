a939d38dbfc499b099df8e81ca94e02f59693175
Arbaja <124392770+Arbaja@users.noreply.github.com>
2024-03-11 18:11:45 -0400

    Update Character.h

--- Character/Character.h
+++ Character/Character.h
@@ -29,7 +29,7 @@ class Character : public Subject{
 public:
     void Attach(Observer* observer);
     void Detach(Observer* observer);
-    void Notify();
+    void Notify(string attribute, int newValue);
     Character(int level); // Constructor declaration
     void printCharacter(); // Method to print character details
     // equip gear
