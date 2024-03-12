6bf784a30578e03c4fffbdbfaa44560c867a5d16
Arbaja <124392770+Arbaja@users.noreply.github.com>
2024-03-11 21:43:22 -0400

    Update Observer.h

--- Observer/Observer.h
+++ Observer/Observer.h
@@ -2,7 +2,10 @@
 class Observer{
     public:
         ~Observer();
-        virtual void Update(string attribute, int newValue, int oldValue) = 0;
+        virtual void Update(string attribute, int newValue, int oldValue) = 0;    
+        virtual void Update(string attribute, int newValue) = 0;
+        virtual void Update(string attribute) = 0;  
+        virtual void Update() = 0;  
     protected:
         virtual Observer();
 };
