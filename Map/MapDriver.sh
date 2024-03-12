12643eac11abf87ea062882b7823c8893b636402
Disputatio <p.felty.howe@gmail.com>
2024-03-12 02:18:04 -0400

    Updated compile bash script

--- Map/MapDriver.sh
+++ Map/MapDriver.sh
@@ -1,2 +1,2 @@
-g++ MapDriver.cpp Map.cpp Campaign.cpp MapEditor.cpp MapEditorCLI.cpp
+g++ MapDriver.cpp Map.cpp Campaign.cpp MapEditor.cpp MapEditorCLI.cpp myEL/MySubject.cpp myEL/MyObserver.cpp myEL/MapObservable.cpp
 ./a.out
