ec8a9677281f480d30c333aa1748d98fdfe60097
Arbaja <124392770+Arbaja@users.noreply.github.com>
2024-03-22 22:38:51 -0400

    Create Consumable.h

---
+++ Character/Consumable.h
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include "Item.h"
+
+
+class Consumable : public Item{
+    public:
+        ~Consumable();
+        Consumable(std::string name, int value);
+        int value;
+        void equip() override; //consume but
+        int getValue();
+
+};
+
