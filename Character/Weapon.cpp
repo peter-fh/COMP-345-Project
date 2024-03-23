b3c9e79c51c579c6990049c3576f12e86dc10193
Arbaja <124392770+Arbaja@users.noreply.github.com>
2024-03-22 22:38:32 -0400

    Create Weapon.cpp

---
+++ Character/Weapon.cpp
@@ -0,0 +1,14 @@
+#include "Weapon.h"
+
+Weapon::Weapon(int base, std::string nom){
+    baseDamage = base;
+    held = false;
+    itemName = nom;
+}
+void Weapon::equip(){held = true;}
+void Weapon::unEquip(){held = false;}
+bool Weapon::equipped(){return held;}
+
+int Weapon::getDamage(){return baseDamage;
+}
+std::string Weapon::getName(){return itemName;}
