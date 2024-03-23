d3f86fdeca7117c1c0665dbfebfc28a3775ad768
Arbaja <124392770+Arbaja@users.noreply.github.com>
2024-03-22 22:37:06 -0400

    Create Corpse.cpp

---
+++ Character/Corpse.cpp
@@ -0,0 +1,17 @@
+#include "Corpse.h"
+
+Corpse::Corpse(Character* c)
+{
+    eulogy = "Corpse of " + c->getName() + ", perished at level: " + std::to_string(c->getLevel());
+    inventory = c->inventory;
+}
+std::string Corpse::getEulogy(){
+    return eulogy;
+}
+
+Item* Corpse::ransack(int i){
+    Item* rt = inventory[i];
+    inventory.erase(inventory.begin() + i);
+    return rt;
+}
+
