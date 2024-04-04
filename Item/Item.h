89e4acaa592bb482b9b3e277cd919ca3225b35f7
Arbaja <124392770+Arbaja@users.noreply.github.com>
2024-04-04 15:28:55 -0400

    Create Item.h

---
+++ Item/Item.h
@@ -0,0 +1,18 @@
+#pragma once
+#ifndef ITEM_H
+#define ITEM_H
+
+#include <iostream>
+#include <string>
+
+class Item{
+    public:
+        std::string itemName;
+        bool held = false;
+        void pickup();
+        void drop();
+        virtual std::string getItemName();
+        virtual void equip();
+};
+
+#endif // ITEM_H
