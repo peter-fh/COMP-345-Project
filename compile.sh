cbebc00ee609de0af501219cdb9dc640799b576b
Arbaja <124392770+Arbaja@users.noreply.github.com>
2024-04-05 18:16:45 -0400

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
 rm log.txt
-g++ -o driver.out -Wno-c++11-extensions main.cpp Observer/Observer.cpp Item/Items.cpp Dice/Dice.cpp Character/Item.cpp Character/Weapon.cpp Character/Consumable.cpp Character/Armor.cpp Character/Corpse.cpp Character/Character.cpp Character/CharacterObserver.cpp Character/PlayerCharacter.cpp Character/CharUI.cpp Character/CharacterBuilder.cpp Map/Map.cpp Map/Campaign.cpp Map/MapEditor.cpp Map/MapEditorCLI.cpp Observer/Logger.cpp
+g++ -o driver.out -Wno-c++11-extensions main.cpp Observer/Observer.cpp Item/Items.cpp Dice/Dice.cpp Item/Item.cpp Item/Weapon.cpp Item/Consumable.cpp Item/Armor.cpp Item/Loot.cpp Enemy/Enemy.cpp Enemy/Corpse.cpp Character/Character.cpp Character/CharacterObserver.cpp Character/PlayerCharacter.cpp Character/CharUI.cpp Character/CharacterBuilder.cpp Map/Map.cpp Enemy/Combat.cpp Map/Campaign.cpp Map/MapEditor.cpp Map/MapEditorCLI.cpp Observer/Logger.cpp
 ./driver.out
 rm driver.out
